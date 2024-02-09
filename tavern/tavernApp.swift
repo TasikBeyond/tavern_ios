b27cc91ac490e1bd425c8f1806feb01566a53934
Brandon Demeria <Tasik.Beyond@gmail.com>
2024-02-09 12:16:03 -0600

    Initial Commit

---
+++ tavern/tavernApp.swift
@@ -0,0 +1,17 @@
+//
+//  tavernApp.swift
+//  tavern
+//
+//  Created by Brandon Demeria on 2024-02-09.
+//
+
+import SwiftUI
+
+@main
+struct tavernApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
